c3119825bba4ad5e2a50d23f30e48f084ca616e8
dkartes <hetyei.kevin@gmail.com>
2022-12-05 16:48:27 +0100

    Ajout des filtres par continent + affichages pays

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,9 +1,58 @@
+import axios from "axios";
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import Card from "./Card";
 
 const Countries = () => {
+  const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(36);
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+  const [selectedRadio, setSelectedRadio] = useState("");
+  // le useEffect se joue lorsque le composant est monté (mis en place)
+  //axios tranforme le fichier json en un fichier JS exploitable
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then(res => setData(res.data));
+  }, []);
   return (
     <div className="countries">
-      <h1>Countries</h1>
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={e => setRangeValue(e.target.value)}
+        />
+        {radios.map(continent => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              name="continentRadio"
+              onChange={e => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
+      </ul>
+      {/* si selectedRadio est true alors tu affiches le boutton */}
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          Annuler la recherche
+        </button>
+      )}
+      <ul>
+        {data
+          .filter(country => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            <Card key={index} country={country} />
+          ))}
+      </ul>
     </div>
   );
 };
