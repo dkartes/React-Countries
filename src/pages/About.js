4e8991bc4e91fa0209cfe7de799165d186e41f74
dkartes <hetyei.kevin@gmail.com>
2022-12-05 10:24:19 +0100

    Projet pays avec React

---
+++ src/pages/About.js
@@ -0,0 +1,38 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sunt maiores
+        quia laudantium dolores, quae ad odio quidem dolor tempore? Facilis
+        saepe laboriosam debitis, amet quos, mollitia accusantium ducimus quas
+        asperiores incidunt harum! Officiis rerum aperiam amet atque animi qui
+        nesciunt accusantium itaque nemo fugiat laborum, similique dolor veniam
+        enim laudantium.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sint quidem
+        aspernatur impedit, optio atque dolores repellat non tempora eveniet
+        ducimus asperiores odit vel inventore mollitia, reprehenderit fugiat
+        repudiandae perferendis. Possimus mollitia voluptates reprehenderit
+        molestias inventore earum hic quasi odit id, sunt ullam et enim, porro
+        omnis, iure tempora deleniti vel distinctio voluptas itaque similique!
+        Dignissimos veniam eos, veritatis cupiditate placeat aperiam harum
+        dolorem corrupti neque autem deserunt in? Ad, ipsum mollitia vitae fugit
+        necessitatibus magni minima maxime doloremque nemo iste, suscipit ea
+        ipsa ex natus incidunt eveniet. Voluptatem nobis praesentium ab, tempora
+        facilis neque nesciunt dolores, blanditiis quia veritatis nisi?
+      </p>
+    </div>
+  );
+};
+
+export default About;
